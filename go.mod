5c582b27f41b7e036f9deb9a9cf754862a8b2de2
thanhdv <thanh.dv.it@gmail.com>
2019-08-29 15:49:36 +0700

    test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.4.1 // indirect
+	github.com/golang/protobuf v1.2.0
 	github.com/thanhdvit/hr v0.0.0-20190829071345-b3504a3b5dce
 	google.golang.org/grpc v1.23.0
 )
--- server.go
+++ server.go
@@ -1,14 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
 	lis "github.com/thanhdvit/hr/db/lis"
-	pd "github.com/thanhdvit/hr/impl"
+	hrpd "github.com/thanhdvit/hr/hello"
 	grpc "google.golang.org/grpc"
 )
+
+type EmployeeServiceServer struct {
+}
+
+func (s *EmployeeServiceServer) ReadBlog(ctx context.Context, req *hrpd.EmployeeReq) (*hrpd.EmployeeRes, error) {
+	response := &hrpd.EmployeeRes{
+		Employee: &hrpd.EmployeeReq{
+			EmployeeId: 1,
+		},
+	}
+	return response, nil
+}
 
 func main() {
 	fmt.Println(lis.ValidUser("002099", "1"))
@@ -18,7 +31,7 @@ func main() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pd.RegisterServiceServer(s, &EmployeeServiceServer{})
+	hrpd.RegisterServiceServer(s, &EmployeeServiceServer{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
